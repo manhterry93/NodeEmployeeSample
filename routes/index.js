0df5c5b294dfb1aef2fda88495489d37139f8a6e
MvP <phammanh.bk56@gmail.com>
2020-12-31 09:28:33 +0000

    setup docker compose

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render(
+    'index',
+    {
+      title: 'Express with Docker',
+      body: `Express is a minimal and flexible Node.js web application framework that 
+      provides a robust set of features for web and mobile applications.`
+    }
+  );
 });
 
 module.exports = router;
