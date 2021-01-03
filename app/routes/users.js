c6263c31edac4eead227426eaeffa573b5f20fc0
MvP-PC <phammanh.bk56@gmail.com>
2021-01-04 01:40:04 +0700

    adding psql

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,14 @@
 var express = require('express');
+const { Client } = require('pg')
+const client = new Client({
+  user:'postgres',
+  host:'localhost',
+  database:'employee',
+  port:'5432'
+});
+
+await client.connect()
+
 var router = express.Router();
 
 /* GET users listing. */
